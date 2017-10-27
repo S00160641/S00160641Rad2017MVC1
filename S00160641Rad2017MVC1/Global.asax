e5e15ab8890e0880694035cb7cc6834eb27e3d31
S00160641 <sophiaprice10@hotmail.com>
2017-10-27 10:57:23 +0100

    Add project files.

---
+++ S00160641Rad2017MVC1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="S00160641Rad2017MVC1.MvcApplication" Language="C#" %>
---
+++ S00160641Rad2017MVC1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(S00160641Rad2017MVC1.Startup))]
+namespace S00160641Rad2017MVC1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
